6d7c0657a427066bd00ec38035d64553b884d4b7
wenqing <wenqing@kerrylan.com>
2023-12-07 11:30:32 +0800

    feat: spacing helper support set spacing times

--- src/views/SpacingView/index.vue
+++ src/views/SpacingView/index.vue
@@ -2,7 +2,14 @@
   <div class="">
     <DemoPanel title="spacing 内外间距辅助" :initialExpand="true">
       <template #description>
-        spacing 辅助类可以帮助你控制元素的内外间距。
+        <div>spacing 辅助类可以帮助你控制元素的内外间距。</div>
+        <div>
+          你可以使用诸如：
+          <code class="lang-css"> mt-*, mb-*, mr-*, ml-*, my-*, mx-* </code>
+          这些辅助类控制外间距。 使用
+          <code class="lang-css"> pt-*, pb-*, pr-*, pl-*, py-*, px-* </code>
+          控制内间距。
+        </div>
       </template>
 
       <template #code>
